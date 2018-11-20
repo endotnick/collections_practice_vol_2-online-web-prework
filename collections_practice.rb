2532faed73c3f4c3e4df2cf09e68d0a19c2bc056
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-20 07:28:34 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
-# your code goes here
+def begins_with_r
+end
+
+def contain_a
+end
+
+def first_wa
+end
+
+def count_elements
+end
+
+def remove_non_strings
+end
+
+def merge_data
+end
+
+def find_cool
+end
+
+def organize_schools
+end
+
